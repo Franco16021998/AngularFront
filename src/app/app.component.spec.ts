6f62a4274ccdf150548a64cce666ec818578e65b
Efrain Bautista <efrain@thibau.io>
2023-09-26 21:14:01 -0500

    updates

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'static-web-ng'`, () => {
+  it(`should have as title 'dynamic-web-ng'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('static-web-ng');
+    expect(app.title).toEqual('dynamic-web-ng');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('static-web-ng app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('dynamic-web-ng app is running!');
   });
 });
